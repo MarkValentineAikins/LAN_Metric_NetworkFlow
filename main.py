73a2a6ff81f114eb996ba1940bf6f660b4132a98
Mark Valentine Aikins <110112381+MarkValentineAikins@users.noreply.github.com>
2023-09-03 18:18:39 +0000

    Update main.py
    I added a function calculate_max_efficient_throughput to estimate the maximum efficient data transmission rate. It uses the network interface's speed, assuming 100 Mbps as the default value. You can change 'eth0' to your specific network interface if needed.
    
    I modified the packet loss calculation to use dropin and dropout fields from net_io_counters to better represent packet loss.
    
    I added error handling when measuring latency to handle exceptions gracefully.
    
    The calculated metrics are now displayed with their appropriate units and labels.
    
    The code also prints the maximum efficient throughput based on the network interface speed.
    
    I added comments for clarity and better understanding of the code flow.

--- main.py
+++ main.py
@@ -5,62 +5,72 @@ import csv
 # Set the duration of monitoring in seconds
 monitoring_duration = 10
 
-# Start monitoring
-start_time = time.time ()
+# Destination IP address or domain for latency measurement
+destination_address = '192.168.8.103'  # Change this to your desired destination
 
 # Lists to store metric samples
-throughput_samples = [ ]
-packet_loss_samples = [ ]
-latency_samples = [ ]
-# net_io.packets_dropped = []
+throughput_samples = []
+packet_loss_samples = []
+latency_samples = []
 
-while time.time () - start_time < monitoring_duration:
+# Function to calculate the maximum efficient data transmission rate
+def calculate_max_efficient_throughput():
+    max_throughput = psutil.net_if_stats().get('eth0', {}).get('speed', 100) * 10**6 / 8  # Assuming 'eth0' as the network interface
+    return max_throughput
+
+# Start monitoring
+start_time = time.time()
+
+while time.time() - start_time < monitoring_duration:
     # Get current network statistics
-    net_io = psutil.net_io_counters ()
+    net_io = psutil.net_io_counters()
 
     # Calculate throughput (bytes per second)
     throughput = net_io.bytes_sent + net_io.bytes_recv
-    throughput_samples.append ( throughput )
+    throughput_samples.append(throughput)
 
     # Calculate packet loss rate
-    packet_loss_rate = net_io.dropout / (net_io.packets_sent + net_io.packets_recv)
-    packet_loss_samples.append ( packet_loss_rate )
+    packet_loss_rate = net_io.dropin + net_io.dropout  # Use dropin and dropout for packet loss
+    packet_loss_samples.append(packet_loss_rate)
 
     # Measure latency (ping to a specific IP or domain)
-    # Example using the ping command (requires the 'ping' executable)
-    # Replace 'example.com' with your desired destination
-    ping_result = psutil.subprocess.Popen ( [ 'ping', '-c', '1', '192.168.8.103' ],
-                                            stdout=psutil.subprocess.PIPE ).communicate ()
-    latency = float ( ping_result [ 0 ].decode ().split ( 'time=' ) [ 1 ].split ( ' ms' ) [ 0 ] )
-    latency_samples.append ( latency )
+    try:
+        ping_result = psutil.subprocess.Popen(['ping', '-c', '1', destination_address], stdout=psutil.subprocess.PIPE).communicate()
+        latency = float(ping_result[0].decode().split('time=')[1].split(' ms')[0])
+        latency_samples.append(latency)
+    except Exception as e:
+        print(f"Error measuring latency: {e}")
 
     # Sleep for a brief interval
-    time.sleep ( 1 )
+    time.sleep(1)
 
 # Calculate average metrics
-average_throughput = sum ( throughput_samples ) / len ( throughput_samples )
-average_packet_loss = sum ( packet_loss_samples ) / len ( packet_loss_samples )
-average_latency = sum ( latency_samples ) / len ( latency_samples )
+average_throughput = sum(throughput_samples) / len(throughput_samples)
+average_packet_loss = sum(packet_loss_samples) / len(packet_loss_samples)
+average_latency = sum(latency_samples) / len(latency_samples)
 
 # Print metrics
-print ( "Average Throughput: {:.2f} bytes/s".format ( average_throughput ) )
-print ( "Average Packet Loss Rate: {:.2f}%".format ( average_packet_loss ) )
-print ( "Average Latency: {:.2f} ms".format ( average_latency ) )
+print("Average Throughput: {:.2f} bytes/s".format(average_throughput))
+print("Average Packet Loss Rate: {:.2f}".format(average_packet_loss))
+print("Average Latency: {:.2f} ms".format(average_latency))
 
+# Calculate maximum efficient data transmission rate
+max_efficient_throughput = calculate_max_efficient_throughput()
+print("Maximum Efficient Throughput: {:.2f} bytes/s".format(max_efficient_throughput))
 
 # Export metrics to CSV file
 def export_metrics_to_csv(throughput_samples, packet_loss_samples, latency_samples):
     filename = "lan_metrics.csv"
 
-    with open ( filename, 'w', newline='' ) as csvfile:
-        writer = csv.writer ( csvfile )
-        writer.writerow ( [ "Throughput (bytes/s)", "Packet Loss Rate (%)", "Latency (ms)" ] )
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Throughput (bytes/s)", "Packet Loss Rate", "Latency (ms)"])
 
-        for i in range ( len ( throughput_samples ) ):
-            writer.writerow ( [ throughput_samples [ i ], packet_loss_samples [ i ], latency_samples [ i ] ] )
+        for i in range(len(throughput_samples)):
+            writer.writerow([throughput_samples[i], packet_loss_samples[i], latency_samples[i]])
 
-    print ( "Metrics exported to {}".format ( filename ) )
-
+    print("Metrics exported to {}".format(filename))
 
 # Export metrics to CSV file
-export_metrics_to_csv ( throughput_samples, packet_loss_samples, latency_samples )
+export_metrics_to_csv(throughput_samples, packet_loss_samples, latency_samples)
+
